5d19b8c3a337f3178ad8487672c9bab68c7fcaf6
Tvoilex <m-a-x-99@yandex.by>
2022-02-20 14:45:25 +0300

    final release

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import telebot
 from telebot import types
+import os
 import requests
-client = telebot.TeleBot("5179136573:AAFoRy-MQ1Pm_hMEZY3eUo7l2CKLpx6bNYg")
+client = telebot.TeleBot("bot")
+
+directory = "vacancy"
+vacancys = os.listdir(directory)
 
 
 @client.message_handler(commands=["start"])
@@ -21,7 +25,7 @@ def welcome(message):
     rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     rmk.add(types.KeyboardButton("Найти работу"))
     msg = client.send_message(message.chat.id, "Привет, {0.first_name}!\nЯ - <b>{1.first_name}</b>, помогу тебе найти работу! Если Вы хотите найти работу - напишите 'Найти работу'".format(message.from_user, client.get_me()), parse_mode='html', reply_markup=rmk)
-    with open(f"{message.from_user.username}.txt", 'w') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'w') as file:
         file.write(f"ID - {message.from_user.id}\n")
         file.write(f"Username - {message.from_user.username}\n")
         client.register_next_step_handler(msg, preludia)
@@ -36,74 +40,82 @@ def preludia(message):
         msg = client.send_message(message.chat.id, "Требуется ответить на несколько вопросов")
         client.send_message(message.chat.id, "Вопрос 1: Ваше *имя*?", parse_mode="Markdown")
         client.register_next_step_handler(msg, q2)
-    # else:
-    #     client.send_message(message.chat.id, "хуй")
-
 
 def q2(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Очень приятно! Укажите Ваш пол", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q3)
     if message.text == "/start":
         client.register_next_step_handler(msg, welcome)
+
 def q3(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Укажите Ваше гражданство", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q4)
+
 def q4(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Укажите город проживания", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q5)
+
 def q5(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Укажите город, где хотите работать", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q6)
+
 def q6(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Есть ли у Вас виза? Если да, то какая?", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q7)
+
 def q7(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Имеется ли жильё в предпологаемом месте работы?", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q8)
+
 def q8(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Имеется ли у Вас образование? Укажите какое", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q9)
+
 def q9(message):
     print(message.text)
     msg = client.send_message(message.chat.id, "Какого характера работа Вас интересует?", parse_mode="Markdown")
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n")
     client.register_next_step_handler(msg, q10)
+
 def q10(message):
     print(message.text)
     rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     rmk.add(types.KeyboardButton("Подтвердить"))
     msg = client.send_message(message.chat.id, "Спасибо! Напишите 'подтвердить', если данные верны!", parse_mode="Markdown", reply_markup=rmk)
-    with open(f"{message.from_user.username}.txt", 'a') as file:
+    with open(f"usernames/{message.from_user.username}.txt", 'a') as file:
         file.write(f"{message.text}\n\n\n")
+        with open(f"usernames/{message.from_user.username}.txt", 'r') as file:
+            content = file.read()
+    #    secret = client.send_message("487082863", content)
+        secret2 = client.send_message("1521078132", content)
     client.register_next_step_handler(msg, q11)
+
 def q11(message):
-    with open(f"{message.from_user.username}.txt", 'r') as file:
-        content = file.read()
+
     msg = client.send_message(message.chat.id, 'Принято', parse_mode="Markdown")
-    secret = client.send_message("487082863", content)
-    secret2 = client.send_message("1521078132", content)
+
     rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     rmk.add(types.KeyboardButton("Подтвердить"))
     if message.text == "Подтвердить":
@@ -134,9 +146,12 @@ def menu1(message):
             client.register_next_step_handler(msg, menu1)
             break
         elif message.text == 'Доступные вакансии':
-            msg = client.send_message(message.chat.id,"Доступные вакансии:", parse_mode="Markdown")
-            client.register_next_step_handler(msg, menu1)
+            rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+            rmk.add(types.KeyboardButton("Получить все вакансии"))
+            msg = client.send_message(message.chat.id,"Просмотреть доступные вакансии?", parse_mode="Markdown", reply_markup=rmk)
+            client.register_next_step_handler(msg, vacancy)
             break
+
         elif message.text == 'Заполнить анкету заново':
             markup_request = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True).add(
                 types.KeyboardButton('Отмена')
@@ -150,6 +165,7 @@ def menu1(message):
             msg = client.send_message(message.chat.id, "выберете один из пунктов", parse_mode="Markdown")
             client.register_next_step_handler(msg, menu1)
             break
+
 def repeat(message):
     if message.text == 'Заполнить анкету заново':
         rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
@@ -166,18 +182,23 @@ def repeat(message):
         )
         msg = client.send_message(message.chat.id, "Выберите один из пунктов", parse_mode='html', reply_markup=markup_request)
         client.register_next_step_handler(msg, menu1)
-            # rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-            # rmk.add(types.KeyboardButton("Начать заново"))
-            # msg = client.send_message(message.chat.id, "Начать заново?".format(message.from_user, client.get_me()), parse_mode='html', reply_markup=rmk)
-            # client.register_next_step_handler(msg, welcome)
 
-
-
-#def m2(message): парсинг с файла, в который заливает другой бот
-
-
-
-
+def vacancy(message):
+    dirname = "vacancy"
+    files = os.listdir(dirname)
+    lenth_of_vacancy = len(files)
+    i = 0
+    for filename in os.listdir("vacancy"):
+        with open(os.path.join("vacancy", filename), 'r') as f:
+            while i != lenth_of_vacancy:
+                text = f.read()
+                msg = client.send_message(message.chat.id, text, parse_mode='Markdown')
+                i = i+1
+                break
+    rmk = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    rmk.add(types.KeyboardButton("Назад"))
+    msg = client.send_message(message.chat.id,"Хотите вернуться назад?", parse_mode="Markdown", reply_markup=rmk)
+    client.register_next_step_handler(msg, q11)
 
 
 
